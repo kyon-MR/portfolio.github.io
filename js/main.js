eee639833ddc1bc8c9e3d9407eb1761b96d41bdc
kyon-MR <108559388+kyon-MR@users.noreply.github.com>
2023-01-22 16:17:38 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
   /* ----------- jQuery ------------ */
   $(function() {
     const swiper = new Swiper(".swiper", {
+      loop: 'true',
 
     pagination: {
       el: '.swiper-pagination',
+      clickable: 'true',
     },
 
     navigation: {
